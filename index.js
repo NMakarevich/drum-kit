08876018a26a443812d3bbaabd304bca76552cb0
Nikolai Makarevich <nikoladze529@mail.ru>
2021-08-05 21:47:14 +0300

    refactor: refactor code; feat: done rec/play events

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
 const audios = Array.from(document.querySelectorAll('audio'));
-  const keysArray = Array.from(document.querySelectorAll('.key'));
-  const keys = document.querySelector('.keys');
-  const recButton = document.querySelector('.button-rec');
-  const playButton = document.querySelector('.button-play');
-  let recArray = [];
-  let isREC = false;
+const keysArray = Array.from(document.querySelectorAll('.key'));
+const keys = document.querySelector('.keys');
+const recButton = document.querySelector('.button-rec');
+const playButton = document.querySelector('.button-play');
+let recArray = [];
+let isREC = false;
 
-  function playSound(event) {
+function keyEvent(event) {
+  if(event.keyCode == 82) rec();
+  else if (event.keyCode == 80 || event.keyCode == 32) playRec();
+  else {
     const audio = audios.find(elem => elem.dataset.key == event.keyCode);
     if (!audio) return;
-    rec(event);
+    recording(event);
     audio.currentTime = 0;
     audio.play();
     switchTransition(event);
   }
+}
 
-  function switchTransition(event) {
-    keysArray.forEach(key => key.classList.remove('playing'));
-    const key = keysArray.find(key => key.dataset.key == event.keyCode);
-    key.classList.add('playing');
-    key.addEventListener('transitionend', evt => {
-      if (evt.propertyName !== 'transform') return;
-      key.classList.remove('playing')
-    }) 
+keys.addEventListener('mousedown', (event) => {
+  const clickedKey = event.target.closest('.key');
+  if (!clickedKey) return;
+  recording(event);
+  const code = {keyCode: clickedKey.dataset.key};
+  keyEvent(code);
+})
+
+function switchTransition(event) {
+  keysArray.forEach(key => key.classList.remove('playing'));
+  const key = keysArray.find(key => key.dataset.key == event.keyCode);
+  key.classList.add('playing');
+  key.addEventListener('transitionend', evt => {
+    if (evt.propertyName !== 'transform') return;
+    key.classList.remove('playing');
+  }) 
+}
+
+document.addEventListener('keydown', keyEvent);
+
+function rec() {
+  isREC = !isREC;
+  recButton.classList.toggle('active');
+  if (isREC) {
+    playButton.disabled = true;
+    playButton.classList.remove('active');
   }
-
-  document.addEventListener('keydown', playSound);
-
-  recButton.addEventListener('click', () => {
-    isREC = !isREC;
-    recButton.classList.toggle('active');
-    if (isREC) {
-      playButton.disabled = true;
-      playButton.classList.remove('active')
-    }
-    else {
+  else {
+    if (recArray.length) {
       playButton.disabled = false;
       playButton.classList.toggle('active');
       recArray = recArray.filter(item => item).map(item => {
@@ -43,32 +56,31 @@ const audios = Array.from(document.querySelectorAll('audio'));
         return obj;
       }).reverse();
     }
-  })
-  
-  playButton.addEventListener('click', () => { 
-    let timerId = setTimeout(function playRec() {
-      if (recArray.length != 0) {
-        playSound(recArray.pop());
-        timerId = setTimeout(playRec, 500);
-      }
-      else {
-        clearTimeout(timerId);
-        playButton.classList.toggle('active');
-        playButton.disabled = !playButton.disabled;
-      }
-    }, 1000);
-  });
- 
-  keys.addEventListener('mousedown', (event) => {
-    const clickedKey = event.target.closest('.key');
-    if (!clickedKey) return;
-    rec(event)
-    const code = {keyCode: clickedKey.dataset.key};
-    playSound(code)
-  })
-
-  function rec(event) {
-    if(isREC) {
-      recArray.push(+event.keyCode)
-    }
   }
+}
+
+function recording(event) {
+  if(isREC) {
+    recArray.push(+event.keyCode);
+  }
+}
+
+function playRec() {
+  let timerId = setTimeout(function playRec() {
+    if (recArray.length != 0) {
+      keyEvent(recArray.pop());
+      timerId = setTimeout(playRec, 500);
+    }
+    else {
+      clearTimeout(timerId);
+      playButton.classList.toggle('active');
+      playButton.disabled = !playButton.disabled;
+    }
+  }, 1000);
+}
+
+recButton.addEventListener('click', rec);
+playButton.addEventListener('click', playRec);
+
+
+
